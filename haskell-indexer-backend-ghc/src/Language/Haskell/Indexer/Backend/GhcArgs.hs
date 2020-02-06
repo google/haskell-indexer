bc5f511d13477f8ed0a9a6090b78550c90fef561
Robin Palotai <palotai.robin@gmail.com>
2020-02-06 23:05:21 +0100

    ghc_kythe_wrapper can now override GHC libdir.
    Normally the libdir is used from ghc-paths, which is the standard libdir
    of the GHC that compiled the tool. But - especially for nix - we might
    want to run the tool with a different libdir, which is of the same GHC
    version, but with more libraries available (via nix `ghcWithPackages`).

--- haskell-indexer-backend-ghc/src/Language/Haskell/Indexer/Backend/GhcArgs.hs
+++ haskell-indexer-backend-ghc/src/Language/Haskell/Indexer/Backend/GhcArgs.hs
@@ -11,29 +11,43 @@
 -- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
-
+{-# Language StrictData #-}
 module Language.Haskell.Indexer.Backend.GhcArgs
     ( GhcArgs(..)
     , ToolOverride(..)
+    , LibdirOverride(..)
     , defaultGhcArgs
     ) where
 
 -- | Args to call GHC with, to perform the compilation.
 data GhcArgs = GhcArgs
-    { gaToolOverride :: !ToolOverride
+    { gaToolOverride :: ToolOverride
     , gaArgs         :: [String]
-    , gaLibdirPrefix :: !FilePath
-      -- ^ Gets prepended to libdir path reported by ghc-paths. Needed if
-      -- runtime location of libdir differs from the reported one.
+    , gaLibdirOverride :: Maybe LibdirOverride
     }
 
 -- | Lets selective override of various tools used during compilation.
 data ToolOverride = ToolOverride
-    { overridePgmP :: !(Maybe FilePath)
+    { overridePgmP :: Maybe FilePath
       -- ^ Override the preprocessor if needed. Only overrides the binary, but
       -- keeps whatever flags were set on it originally (from the GHC command
       -- line).
     }
 
+-- | The libdir determines the default libraries available to GHC. Normally it
+-- is determined by calling a ghc-paths library function, but under special
+-- circumstances one might want to override it.
+data LibdirOverride
+    = AddPrefixToLibdir FilePath
+      -- ^ Prepend prefix to libdir.
+    | OverrideLibdir FilePath
+      -- ^ Explicit override.
+
 defaultGhcArgs :: GhcArgs
-defaultGhcArgs = GhcArgs (ToolOverride Nothing) [] ""
+defaultGhcArgs = GhcArgs
+    { gaToolOverride = ToolOverride
+        { overridePgmP = Nothing
+        }
+    , gaArgs = []
+    , gaLibdirOverride = Nothing
+    }
